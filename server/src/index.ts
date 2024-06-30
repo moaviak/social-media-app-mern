791470053bedc57c1cbb287aa8fb01538f1a0598
moavia <mmoavia2002@gmail.com>
2024-06-30 22:07:04 +0500

    refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,7 +41,7 @@ app.use("/api/posts", postRoutes);
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
+    res.sendFile(path.join(__dirname, "..", "views", "404.html"));
   } else if (req.accepts("json")) {
     res.json({ message: "404 Not Found" });
   } else {
